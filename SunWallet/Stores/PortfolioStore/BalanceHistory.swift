50b11bd35ebaaaea2df73af9cbe3a7c69d1f3c0f
Alex Ilin <nightwill@users.noreply.github.com>
2020-07-27 19:35:49 +0300

    Fix bugs with converting history values

--- SunWallet/Stores/PortfolioStore/BalanceHistory.swift
+++ SunWallet/Stores/PortfolioStore/BalanceHistory.swift
@@ -5,29 +5,30 @@ struct BalanceHistory {
     
     init(walletInfo: WalletInfo) {
         var currentBalance = walletInfo.balance
-        self.historyValues = walletInfo.transactions
+        let transactionValues = walletInfo.transactions
             .reversed()
             .map { transaction -> HistoryValue in
-                currentBalance -= transaction.value
+                defer { currentBalance -= transaction.value }
                 return HistoryValue(date: transaction.date, value: currentBalance)
             }
             .reversed()
+        let initialValue = HistoryValue(date: Date(timeIntervalSince1970: 0), value: currentBalance)
+        self.historyValues = [initialValue] + transactionValues
     }
     
     func converted(historySet: HistorySet) -> HistorySet {
         .init(
             hourly: converted(historyValues: historySet.hourly),
-            daily: converted(historyValues: historySet.hourly),
-            weekly: converted(historyValues: historySet.hourly),
-            monthly: converted(historyValues: historySet.hourly),
-            yearly: converted(historyValues: historySet.hourly),
-            all: converted(historyValues: historySet.hourly)
+            daily: converted(historyValues: historySet.daily),
+            weekly: converted(historyValues: historySet.weekly),
+            monthly: converted(historyValues: historySet.monthly),
+            yearly: converted(historyValues: historySet.yearly),
+            all: converted(historyValues: historySet.all)
         )
     }
     
     private func balance(at date: Date) -> Double {
-        //TODO: - Need test
-        historyValues.first(where: { date > $0.date })?.value ?? 0
+        historyValues.last(where: { date < $0.date })?.value ?? 0
     }
     
     private func converted(historyValues: [HistoryValue]) -> [HistoryValue] {
