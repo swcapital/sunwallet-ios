55b886a6effa44e98aa648c88c6a0bd0de6db1d2
Alex Ilin <nightwill@users.noreply.github.com>
2020-07-27 17:33:17 +0300

    Fix loading transaction from Blockchair

--- SunWallet/Repositories/BlockchainRepository/BlockchairBlockchainRepository.swift
+++ SunWallet/Repositories/BlockchainRepository/BlockchairBlockchainRepository.swift
@@ -6,6 +6,12 @@ private let token = "PA__A7AmX35RoCXNBMUTKsR2xVR5u7IB"
 
 struct BlockchairBlockchainRepository: BlockchainRepository {
     
+    private var decoder: JSONDecoder {
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .formatted(.bcDateFormatter)
+        return decoder
+    }
+    
     func balance(for wallet: Wallet) -> AnyPublisher<WalletInfo, Error> {
         switch wallet.asset {
         case .btc:
@@ -22,11 +28,12 @@ struct BlockchairBlockchainRepository: BlockchainRepository {
 extension BlockchairBlockchainRepository {
     
     private func bitcoinBalance(address: String) -> AnyPublisher<WalletInfo, Error> {
-        let address = "\(host)/bitcoin/dashboards/address/\(address)" + "?limit=1"
+        let address = "\(host)/bitcoin/dashboards/address/\(address)" + "?limit=1&transaction_details=true"
         let url = URL(string: address)!
         return URLSession.shared.dataTaskPublisher(for: url)
             .extractData()
-            .decode(type: BalancesResponse<BitcoinData>.self, decoder: JSONDecoder())
+            .decode(type: BalancesResponse<BitcoinData>.self, decoder: decoder)
+            .print()
             .map {
                 let data = $0.data.first!.value
                 let balance = data.address.balance ?? 0
@@ -35,15 +42,15 @@ extension BlockchairBlockchainRepository {
                     .map { Transaction(date: $0.time, value: $0.balanceChange) }
                     .reversed()
                 return WalletInfo(balance: balance, transactions: Array(transactions))
-            }
-            .eraseToAnyPublisher()
+        }
+        .eraseToAnyPublisher()
     }
     
     private struct BitcoinData: Codable {
         let address: BitcoinBalance
         let transactions: [BitcoinTransaction]
     }
-
+    
     private struct BitcoinBalance: Codable {
         let balance: Double?
         let balanceUSD: Double
@@ -53,7 +60,7 @@ extension BlockchairBlockchainRepository {
             case balanceUSD = "balance_usd"
         }
     }
-
+    
     private struct BitcoinTransaction: Codable {
         let time: Date
         let balanceChange: Double
@@ -73,26 +80,27 @@ extension BlockchairBlockchainRepository {
         let url = URL(string: address)!
         return URLSession.shared.dataTaskPublisher(for: url)
             .extractData()
-            .decode(type: BalancesResponse<EtheriumData>.self, decoder: JSONDecoder())
+            .decode(type: BalancesResponse<EtheriumData>.self, decoder: decoder)
+            .print()
             .map {
                 let data = $0.data.first!.value
-                let balance = data.address.balance ?? 0
+                let balance = data.address.balance.map { Double($0) ?? 0 } ?? 0
                 let transactions = data.calls
                     .sorted(by: { $0.time > $1.time })
                     .map { Transaction(date: $0.time, value: $0.value) }
                     .reversed()
                 return WalletInfo(balance: balance, transactions: Array(transactions))
-            }
-            .eraseToAnyPublisher()
+        }
+        .eraseToAnyPublisher()
     }
     
     private struct EtheriumData: Codable {
         let address: EtheriumBalance
         let calls: [EtheriumCall]
     }
-
+    
     private struct EtheriumBalance: Codable {
-        let balance: Double?
+        let balance: String?
         let balanceUSD: Double
         
         enum CodingKeys: String, CodingKey {
@@ -100,7 +108,7 @@ extension BlockchairBlockchainRepository {
             case balanceUSD = "balance_usd"
         }
     }
-
+    
     private struct EtheriumCall: Codable {
         let time: Date
         let value: Double
@@ -114,4 +122,12 @@ extension BlockchairBlockchainRepository {
 
 private struct BalancesResponse<T: Codable>: Codable {
     let data: [String: T]
+}
+
+extension DateFormatter {
+    static let bcDateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        return formatter
+    }()
 }
